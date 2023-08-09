2ec4e4ab3d312b60ebfa0a9fc20d1396c4421f99
Carter McMahon <carterwmcmahon@gmail.com>
2023-08-09 15:41:09 -0700

    trying to bring eww up to date attempt 3

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -140,7 +140,7 @@
     font-manager
     git
     gh
-    eww = pkgs.eww-wayland.overrideAttrs ( finalAttrs: previousAttrs: { 
+    eww-wayland.overrideAttrs ( finalAttrs: previousAttrs: { 
       src = fetchFromGitHub {
         owner = "elkowar";
         repo = "eww";
@@ -149,7 +149,7 @@
       };
 
       cargoHash = lib.fakeHash;
-    });
+    })
     jq
     python3
   ];
