d8912e6924f8e1c273785aea402067e110c497eb
Carter McMahon <carterwmcmahon@gmail.com>
2023-08-10 11:21:24 -0700

    hyprland config in home manager 2

--- flake.nix
+++ flake.nix
@@ -30,13 +30,11 @@
 
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
-    homeConfigurations = {
-      "carterm@cwm-nixos" = home-manager.lib.homeManagerConfiguration {
+    homeConfigurations."carterm@cwm-nixos" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
         # > Our main home-manager configuration file <
         modules = [ ./home-manager/home.nix ];
-      };
     };
   };
 }
