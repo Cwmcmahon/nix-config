85ff4e1ed13ec80494389b4aa163410a5708188f
Carter McMahon <carterwmcmahon@gmail.com>
2023-08-11 10:53:00 -0700

    brightness control

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -120,7 +120,7 @@
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
       ];
-      extraGroups = [ "networkmanager" "wheel" ];
+      extraGroups = [ "networkmanager" "wheel" "video" ];
     };
   };
 
@@ -149,6 +149,9 @@
     ];
     enableDefaultFonts = true;
   };
+
+  # Brightness control
+  programs.light.enable = true;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
