a30caacf81ae02ac81009a1a8a232cc2b3de2d25
Carter McMahon <carterwmcmahon@gmail.com>
2023-08-11 12:13:18 -0700

    comment changes and nm indicator

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,29 +1,14 @@
 { inputs, lib, config, pkgs, ... }: {
   # You can import other home-manager modules here
   imports = [
-    # If you want to use home-manager modules from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModule
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
   ];
 
   nixpkgs = {
     # You can add overlays here
     overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
     ];
     # Configure your nixpkgs instance
     config = {
-      # Disable if you don't want unfree packages
       allowUnfree = true;
       # Workaround for https://github.com/nix-community/home-manager/issues/2942
       allowUnfreePredicate = (_: true);
@@ -36,10 +21,6 @@
     homeDirectory = "/home/carterm";
   };
 
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
-
   # Hyprland config
   wayland.windowManager.hyprland = {
     enable = true;
@@ -47,7 +28,7 @@
     xwayland.enable = true;
     settings = {
       monitor = "eDP-1,1920x1080,auto,1";
-      exec-once = "wbg ~/Pictures/Wallpapers/mult_falls.jpg & waybar & dunst & blueman-applet & nm-applet";
+      exec-once = "wbg ~/Pictures/Wallpapers/mult_falls.jpg & waybar & dunst & blueman-applet & nm-applet --indicator";
       env = "XCURSOR_SIZE,24";
       input = {
         kb_layout = "us";
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,29 +1,12 @@
 { inputs, lib, config, pkgs, ... }: {
   # You can import other NixOS modules here
   imports = [
-    # If you want to use modules from other flakes (such as nixos-hardware):
-    # inputs.hardware.nixosModules.common-cpu-amd
-    # inputs.hardware.nixosModules.common-ssd
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./users.nix
-
-    # Import your hardware configuration
     ./hardware-configuration.nix
   ];
 
   nixpkgs = {
     # You can add overlays here
     overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
     ];
     config = {
       allowUnfree = true;
