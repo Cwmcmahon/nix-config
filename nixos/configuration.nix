f6e9aa81fc8dae4bda9625571493b613c3640f1f
Carter McMahon <carterwmcmahon@gmail.com>
2023-08-10 18:05:34 -0700

    font fixes

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -139,13 +139,16 @@
     font-manager
     git
     gh
-    hack-font
   ];
 
   # Fonts!
-  fonts.packages = with pkgs; [
-    font-awesome
-  ];
+  fonts = {
+    packages = with pkgs; [
+      font-awesome
+      hack-font
+    ];
+    enableDefaultFonts = true;
+  };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
