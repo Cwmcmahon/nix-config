5a6f5664490d6bad051ec37e9c43650bef120336
Carter McMahon <carterwmcmahon@gmail.com>
2023-09-26 16:07:25 -0700

    added link to nix-profile bin

--- nixos/install_silverbullet.nix
+++ nixos/install_silverbullet.nix
@@ -7,5 +7,6 @@ pkgs.writeShellApplication {
 	
 	text = ''
 	deno install -f --name silverbullet --unstable -A https://get.silverbullet.md
+	ln -s ~/.deno/bin/silverbullet ~/.nix-profile/bin/silverbullet
 	'';
 }
