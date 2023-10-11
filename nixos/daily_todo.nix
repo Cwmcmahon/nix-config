99d1f3000df9877618a850ed984bd78a1271e797
Carter McMahon <carterwmcmahon@gmail.com>
2023-10-11 12:55:06 -0700

    make daily_todo script configurable

--- nixos/daily_todo.nix
+++ nixos/daily_todo.nix
@@ -1,4 +1,4 @@
 { pkgs }:
 	pkgs.writeShellScriptBin "daily_todo" ''
-		${pkgs.nb}/bin/nb todo add --due $(date +%F) --description "Daily todo for $(date +%F)" --task "Take meds" --task "Shower" --task "Do laundry" --task "Make bed" --task "Setup scene" --task "Do dishes" --task "Job search"
+		${pkgs.nb}/bin/nb todo add --due ''${1:-$(date +%F)} --description "Daily todo for ''${1:-$(date +%F)}" --task "Take meds" --task "Shower" --task "Do laundry" --task "Make bed" --task "Setup scene" --task "Do dishes" --task "Job search"
 	''
