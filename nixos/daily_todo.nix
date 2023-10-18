69a011833779a1d95923a5de438a004b9d0477b0
Carter McMahon <carterwmcmahon@gmail.com>
2023-10-18 11:51:32 -0700

    daily_todo.nix remove tabs

--- nixos/daily_todo.nix
+++ nixos/daily_todo.nix
@@ -1,9 +1,9 @@
 { pkgs }:
 	pkgs.writeShellScriptBin "daily_todo" ''
 		todo.sh addm "(A) Take meds @home +daily due:$(date +%F)
-		(A) Brush teeth AM @home +daily due:$(date +%F)
-		(B) Make bed @home +daily due:$(date +%F)
-		(B) Set up scene @home +daily due:$(date +%F)
-		(B) Do dishes @home +daily due:$(date +%F)
-		(A) Brush teeth PM @home +daily due:$(date +%F)"
+(A) Brush teeth AM @home +daily due:$(date +%F)
+(B) Make bed @home +daily due:$(date +%F)
+(B) Set up scene @home +daily due:$(date +%F)
+(B) Do dishes @home +daily due:$(date +%F)
+(A) Brush teeth PM @home +daily due:$(date +%F)"
 	''
