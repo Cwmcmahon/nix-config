ad56bbe6598798c6373ce075d09ef4ad5f894398
Carter McMahon <carterwmcmahon@gmail.com>
2023-10-20 11:38:24 -0700

    hyprland blur enable -> enabled

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -35,7 +35,7 @@
 			decoration = {
 				rounding = 10;
 				blur = {
-					enable = true;
+					enabled = true;
 					size = 3;
 					passes = 1;
 					new_optimizations = true;
@@ -49,12 +49,12 @@
 				enabled = "yes";
 				bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
 				animation = [ 
-				"windows, 1, 7, myBezier"
-				"windowsOut, 1, 7, default, popin 80%"
-				"border, 1, 10, default"
-				"borderangle, 1, 8, default"
-				"fade, 1, 7, default"
-				"workspaces, 1, 6, default"
+					"windows, 1, 7, myBezier"
+					"windowsOut, 1, 7, default, popin 80%"
+					"border, 1, 10, default"
+					"borderangle, 1, 8, default"
+					"fade, 1, 7, default"
+					"workspaces, 1, 6, default"
 				];
 			};
 			dwindle = {
