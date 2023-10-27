46ab42ba058a4f66c70906be58f0dd5170468f10
Carter McMahon <carterwmcmahon@gmail.com>
2023-10-27 11:41:42 -0700

    open-notes alias

--- home-manager/aliases.nix
+++ home-manager/aliases.nix
@@ -10,10 +10,11 @@
 (B) Set up scene @home +daily due:$(date +%F)
 (B) Do dishes @home +daily due:$(date +%F)
 (A) Brush teeth PM @home +daily due:$(date +%F)"'';
-    commit-notes = ''(cd ~/Documents/workbench/ && git add -A && git commit -am "$(date)")'';
+		commit-notes = ''(cd ~/Documents/workbench/ && git add -A && git commit -am "$(date)")'';
 		push-notes = ''(cd ~/Documents/workbench/ && git push)'';
 		pull-notes = "(cd ~/Documents/workbench/ && git pull)";
 		nix-switch = "sudo nixos-rebuild switch --flake ~/Documents/nix-config/#cwm-nixos";
 		hm-switch = "home-manager switch --flake ~/Documents/nix-config/#carterm@cwm-nixos";
+		open-notes = "codium ; sleep 2 ; codium ~/Documents/workbench/README.md"
 	};
 }
