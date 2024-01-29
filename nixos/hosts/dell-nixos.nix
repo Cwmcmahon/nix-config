6fc42f8fa321e3fa63a2e50bb9b010cfbeb9dccd
Carter McMahon <carterwmcmahon@gmail.com>
2024-01-28 16:22:58 -0800

    remove impermanence from dell config

--- nixos/hosts/dell-nixos.nix
+++ nixos/hosts/dell-nixos.nix
@@ -1,45 +1,16 @@
-{ lib, ... }: {
+{ ... }: {
   imports = [
     ../common.nix
   ];
 
   # Networking
   networking = {
-    hostName = "dell";
+    hostName = "dell-nixos";
     networkmanager.enable = true;
   };
 
-  environment.persistence."/persistent" = {
-    hideMounts = true;
-    directories = [
-      "/var/log"
-      "/var/lib/systemd/coredump"
-      "/var/lib/bluetooth"
-      "/etc/NetworkManager/system-connections"
-      "/etc/ssh"
-    ];
-    users.carterm = {
-      directories = [
-        "Documents"
-        "Downloads"
-        "Music"
-        "Pictures"
-        "Videos"
-        { directory = ".ssh"; mode = "0700"; }
-        { directory = ".local/share/keyrings"; mode = "0700"; }
-      ];
-      files = [
-        ".gitconfig"
-      ];
-    };
-  };
-
-  fileSystems = {
-    "/home".neededForBoot = true;
-    "/persist".neededForBoot = true;
-  };
-
-  boot.initrd.postDeviceCommands = lib.mkAfter ''
-    zfs rollback -r rpool/local/root@blank
-  '';
+  swapDevices = [ {
+      device = "/var/lib/swapfile";
+      size = 16*1024;
+  } ];
 }
