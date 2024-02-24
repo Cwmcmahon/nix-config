b6c946cf063bb712f4b3083476003021cfdb0092
Carter McMahon <carterwmcmahon@gmail.com>
2024-02-24 09:29:54 -0800

    add python flake and remove nixvim flake

---
+++ dev-envs/python/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A Nix-flake-based Python development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          (python3.withPackages(ps: with ps; [
+            jupyter 
+            ipython 
+            numpy 
+            pandas 
+            matplotlib 
+            seaborn 
+          ]))
+        ];
+        shellHook = "jupyter notebook";
+      };
+    }
+  );
+}
